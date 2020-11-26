74b81ba75a1d74821b40151d6d3295c38cd1cd9b
Miguel Jimenez <majimenezbarros@gmail.com>
2020-11-26 09:25:44 -0500

    Quitar tiburón

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -4,7 +4,7 @@ public class GeneradorVertebrados {
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero"); 
         miVertebrados[2] = new Vertebrado("Paloma", "Ave"); 
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
+        //miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
         
         return miVertebrados;
     }
